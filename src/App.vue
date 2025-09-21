bc2afee50fded849ff1c296b29bcf22df5da39c3
Ashif9683 <ashif111200@gmail.com>
2025-09-21 11:17:27 +0530

    First Day Of Vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue 3!</h1>
+  </div>
+  <Home/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+
+  name: 'App',
+
+  components: {
+       Home,
+  }
+}
+
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <h1 class="box">Home Component</h1>
+
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<style scoped>
+.box {
+    background-color: var(--primary-color);
+    color: var(--secondary-color);
+    padding: 10px;
+}
+</style>
