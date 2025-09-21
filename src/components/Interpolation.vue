9834cd0f8f0a4ce5de1a5e3af5961082e19cc5b2
musafiryatra15 <musafiryatra15@gmail.com>
2025-09-21 11:41:45 +0530

    Interpolation and data()

---
+++ src/components/Interpolation.vue
@@ -0,0 +1,30 @@
+<template>
+
+<h1>Interpolation</h1>
+
+<h2>User Details</h2>
+<ul>
+    <li>Name  : {{ name }}</li>
+    <li>Email : {{ email }}</li>
+    <li>Mobile : {{ getNumber() }}</li>
+</ul>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Interpolation',
+
+    data(){
+        return{
+            name: 'Ashif',
+            email: 'ashif@gmail.com',
+            getNumber: function(){
+                return '7706012785';
+            }
+        }
+    }
+}
+
+</script>
