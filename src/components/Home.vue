a9c8781af07d81609128b5a7d01e392da123b0ed
Ashif9683 <ashif111200@gmail.com>
2025-09-27 18:15:04 +0530

    Methods Property

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,41 @@
 <template>
 
-    <h1 class="box">Home Component</h1>
-
+    <!-- <h1 class="box">Home Component</h1> -->
+    <h2>All Details : </h2>
+    <ul>
+        <li>Name : {{ name }}</li>
+        <li>Age : {{ age }}</li>
+        <li>Role : {{ getRole("Trainee") }}</li>
+        <li>Company Name : {{ getCompany().company }}</li>
+        <li>Company Address : {{ getCompany().address }}</li>
+    </ul>
 </template>
 
 <script>
 export default {
     name: 'Home',
+
+    data() {
+        return {
+            name: 'Ashif',
+            age: 23,
+            company: 'Webkul',
+            address: "Noida sector 63",
+        }
+    },
+
+    methods: {
+        getRole(role) {
+            return role;
+        },
+
+        getCompany() {
+            return {
+                company: this.company,
+                address: this.address,
+            }
+        }
+    }
 }
 </script>
 
