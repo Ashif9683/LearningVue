83f5a513a7c9022cdd030cae5cb2e9c42c5e6fe4
Ashif9683 <ashif111200@gmail.com>
2025-09-29 23:09:54 +0530

    Two way Binding

---
+++ src/components/CheckBoxRadio.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <h1 class="box">CheckBox and Radio</h1>
+
+    <h3>Who I am</h3>
+
+    <label for="developer">Developer</label>
+    <input type="checkbox" value="developer" v-model="who" id="developer">
+
+    <label for="tester">Tester</label>
+    <input type="checkbox" value="tester" v-model="who" id="tester" />
+
+    <label for="designer">Designer</label>
+    <input type="checkbox" value="designer" v-model="who"  id="designer">
+
+    <h3>Technology</h3>
+    <label for="laravel">Laravel</label>
+    <input type="radio" value="laravel" v-model="technology" id="laravel">
+
+    <label for="vue">Vue</label>
+    <input type="radio" value="vue" v-model="technology" id="vue">
+
+    <h3 class="box">I am {{ who }} and used {{ technology }}</h3>
+
+</template>
+
+<script>
+export default {
+
+    name: "CheckBoxRadio",
+
+    data() {
+        return {
+            technology:null,
+            who:[]
+        }
+
+    }
+}
+
+</script>
---
+++ src/components/TwoInputs.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <h1 class="box">Two inputs Two way binding component</h1>
+
+    <h2>Enter Details</h2>
+    <input type="text" v-model="name" placeholder="Enter your name"/>
+    <br><br>
+    <input type="email" v-model="email" placeholder="Enter your email"/>
+    <br><br>
+
+    <ul>
+        <li><strong>Name:</strong> {{ name }}</li>
+        <li><strong>Email:</strong> {{ email }}</li>
+    </ul>
+
+
+
+</template>
+
+<script>
+
+export default {
+    name: 'TwoInputs',
+
+    data(){
+        return {
+            name: '',
+            email: '',
+        }
+    }
+    
+}
+
+</script>
+
+
+<style scoped>
+.box {
+    background-color: var(--primary-color);
+    color: var(--secondary-color);
+    padding: 10px;
+}
+</style>
