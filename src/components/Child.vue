befcdfa5272d81bcff85f01499d75697474ecdac
Ashif9683 <ashif111200@gmail.com>
2025-10-02 11:43:43 +0530

    Pass Data Parent to Child

---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2 class="box">Child Component</h2>
+
+    <h3>Details</h3>
+
+    <ul>
+        <li>Name : {{ name }}</li>
+        <li>Role : {{ user.role }}</li>
+        <li>Company : {{ user.company }}</li>
+        <li>Technology : {{ getTech ().join(', ')}}</li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    name: "Child",
+
+    props: {
+        name: String,
+        user: Object,
+        getTech: Function,
+    }
+}
+
+</script>
