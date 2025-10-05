c2de5307be1889b0b9533c7a522f6a2cc6a0a10a
Ashif9683 <ashif111200@gmail.com>
2025-10-05 10:08:35 +0530

    remove

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,14 @@
   <div id="app">
     <h1>Hello Vue 3!</h1>
   </div>
-  <Methods/>
 </template>
 
 <script>
-import Methods from './components/Methods.vue'
 
 export default {
 
   name: 'App',
-
-  components: {
-       Methods,
-  }
+  
 }
 
 </script>
