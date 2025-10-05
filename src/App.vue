4ba3253ad3fd03af2a874218bf3829577113931d
Ashif9683 <ashif111200@gmail.com>
2025-10-05 12:21:33 +0530

    Home

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <h1>Hello Vue 3!</h1>
+    <Home />
   </div>
 </template>
 
 <script>
+import Home from './components/Home.vue'
 
 export default {
-
   name: 'App',
-  
+  components: {
+    Home
+  }
 }
-
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="home">
+        <h2>Welcome to the Home Page</h2>
+        <p>This is the home page of our Vue.js application.</p>
+    </div>
+</template>
+<script>    
+
+</script>
