9113a17546723447fb05dc5a60b389b57961bbda
Ashif9683 <ashif111200@gmail.com>
2025-10-12 10:51:04 +0530

    CSS

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,18 @@ export default {
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  background: #f5f6fa;
+  min-height: 100vh;
+  padding: 40px;
+}
+
+h1 {
+  color: #42b983;
+  margin-bottom: 24px;
+}
+</style>
