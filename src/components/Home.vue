2245aa21a3c9b6faf2d5d4dc99a659fee0567fc3
Ashif9683 <ashif111200@gmail.com>
2025-11-30 17:25:59 +0530

    Modifiers

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,31 @@
     <div class="home">
         <h2>Welcome to the Home Page</h2>
         <p>This is the home page of our Vue.js application.</p>
+
+        <h3>Form Modifiers</h3>
+        <label>Name : </label>
+        <input type="text" v-model.lazy="name">
+        <br><br>
+        <label>Age : </label>
+        <input type="number" v-model.number="age">
+        <br><br>
+        <ul>
+            <li>Your Name is : {{ name }}</li>
+            <li>Your age is : {{ age }}</li>
+        </ul>
     </div>
 </template>
 <script>    
+
+export default {
+    name : 'Home',
+
+    data() {
+        return {
+            name: '',
+            age: null,
+        }
+    }
+}
 
 </script>
