18f25378b638ec3fe708a57c1be9993f17baed16
Ashif9683 <ashif111200@gmail.com>
2025-11-30 18:56:37 +0530

    Watch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,26 @@
     <div class="home">
         <h2>Welcome to the Home Page</h2>
         <p>This is the home page of our Vue.js application.</p>
+        <h3>Count : {{ count }}</h3>
+        <button v-on:click="count += 1"> + </button><br><br>
+        <button v-on:click="count -= 1"> - </button>
     </div>
 </template>
 <script>    
+export default {
+    name: 'Home',
+    data() {
+        return {
+            count: 0
+        };
+    },
 
+    watch: {
+        count(val) {
+            if (val > 5 || val < 0) {
+                this.count = 0;
+            }
+        }
+    }
+};
 </script>
