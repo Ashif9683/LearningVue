9423c09086a2b2800ce877ec556a4abe567d2339
Ashif9683 <ashif111200@gmail.com>
2025-11-30 18:08:21 +0530

    Computed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,25 @@
     <div class="home">
         <h2>Welcome to the Home Page</h2>
         <p>This is the home page of our Vue.js application.</p>
+        <h3>Computed Property</h3>
+        <p>Reversed Message: {{ reversedMessage }}</p>
     </div>
 </template>
 <script>    
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            message: 'Hello, Vue.js!'
+        }
+    },
+
+    computed: {
+        reversedMessage() {
+            return this.message.split('').reverse().join('');
+        }
+    }
+}
 
 </script>
