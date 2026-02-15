110c8f0082551d4754cd831a13458899cb112f57
Ashif9683 <ashif111200@gmail.com>
2026-02-15 17:45:00 +0530

    404 not found

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <router-link to="/">Go back to Home</router-link>
+    </div>
+</template>
+
+<script>    
+export default {
+    name: 'PageNotFound',
+    data() {
+        return {
+            message: '404 - Page Not Found'
+        }
+    }
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue"
 import Profile from "./components/Profile.vue"
 import Login from "./components/Login.vue";
+import PageNotFound from "./components/PageNotFound.vue";
 
 const routes = [
     { name: "home", path: '/', component: Home},
     { name: "login", path: '/login', component: Login},
     { name: "profile", path: '/profile/:name', component: Profile},
+    { name: "not-found", path: '/:pathMatch(.*)*', component: PageNotFound},
 ];
 
 const router = createRouter({
